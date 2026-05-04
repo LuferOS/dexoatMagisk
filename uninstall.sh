c2d69bbcec35a7e86542f8726037a66a20dfa716
LuferOS <luisfernandoguzmannino@gmail.com>
2026-05-04 06:14:43 -0500

    Enhance uninstall script with logging and cleanup
    Add logging and cleanup delegation to Magisk during uninstallation

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,5 @@
 #!/system/bin/sh
 # uninstall.sh
-MODULE_DIR="/data/adb/modules/dexopt-boost"
-rm -f "$MODULE_DIR/.ran_once"
+# Magisk elimina la carpeta del módulo automáticamente, incluyendo el .ran_once.
+log -p i -t LuferOS_Dex2OAT "Módulo desinstalado. Limpieza delegada a Magisk."
+exit 0
