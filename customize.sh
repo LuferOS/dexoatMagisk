49e2cd25e800725adfad5196f3d7ca372ad73151
LuferOS <luisfernandoguzmannino@gmail.com>
2026-05-04 06:12:48 -0500

    Add customize.sh for LuferOS Dex2OAT Optimizer

---
+++ customize.sh
@@ -0,0 +1,10 @@
+#!/sbin/sh
+ui_print "*****************************************"
+ui_print "* 🔥 LuferOS Dex2OAT Optimizer          *"
+ui_print "* ⚡ Inyectando esteroides al Exynos... *"
+ui_print "*****************************************"
+
+set_perm $MODPATH/service.sh 0 0 0755
+ui_print "- Permisos de ejecución asegurados."
+ui_print "- Reinicia para comenzar la compilación."
+exit 0
